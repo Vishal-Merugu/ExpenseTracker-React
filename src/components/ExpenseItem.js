ff59108a9ccbb4d44c2246b92dba40f26963ca35
Vishal-Merugu <buntymerugu0@gmail.com>
2023-07-15 18:04:56 +0530

    Added h2 Expense Items and li Expenses

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem(){
+    return <h3>Expense Items !!</h3>
+}
+
+export default ExpenseItem
---
+++ src/components/Expenses.js
@@ -0,0 +1,11 @@
+function Expenses(){
+    return (
+        <ul>
+            <li>Food Rs.10</li>
+            <li>Petrol Rs.100</li>
+            <li>Movie Rs.200</li>
+        </ul>
+    )
+}
+
+export default Expenses
