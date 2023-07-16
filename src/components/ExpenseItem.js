d2aa0a092a59a77da2e9cb48d7e6899b4dd68eda
Vishal-Merugu <buntymerugu0@gmail.com>
2023-07-16 23:16:35 +0530

    import react and createelement() old school

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,31 @@
+import React from "react";
+
 import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate"
+import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "./Card";
 
 function ExpenseItem(props) {
-  
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
-      <ExpenseDetails title={props.title} amount={props.amount} locationOfExpense={props.locationOfExpense}/>
-    </Card>
+  return React.createElement(
+    Card,
+    { className: "expense-item" },
+    React.createElement(ExpenseDetails, {
+      title: props.title,
+      amount: props.amount,
+      locationOfExpense: props.locationOfExpense,
+    })
   );
+
+  // return (
+  //   <Card className="expense-item">
+  //     <ExpenseDate date={props.date} />
+  //     <ExpenseDetails
+  //       title={props.title}
+  //       amount={props.amount}
+  //       locationOfExpense={props.locationOfExpense}
+  //     />
+  //   </Card>
+  // );
 }
 
 export default ExpenseItem;
